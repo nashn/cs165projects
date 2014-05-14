f1fc82507e2c8d2e023598d2a2dbcd8c9e632713
nashn <nies@uci.edu>
2014-05-14 12:36:10 -0700

    Create test_output.cpp

---
+++ cs165_project_3/test_output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+        cout << "filename     " << "news"<< endl;
+        cout << "size         " << 123 << endl;
+
+        cout << "----" << endl;
+        cout << "HUFFsiz      " << 12 << endl;
+        cout << "%save        " << (double)12 / 123 << endl;
+        cout << "t-cmprss     " << 0 << endl;
+        cout << "t-expand     " << 0 << endl;
+
+
+        return 0;
+}
